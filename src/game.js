f3af59df7c8bb7876edaba26c6e4e788e8a2537c
Jack Johnson <jrj221@byu.edu>
2025-10-13 17:41:47 -0600

    Set up a reset button for testing, and functionality for one building to increase CPS. This will likely be turned into a more generic function later on

--- src/game.js
+++ src/game.js
@@ -1,17 +1,35 @@
-import { initBuildingHover, initCoin, initSaveSystem } from "./initializers";
+import { initBuildingHover, initCoin, initSaveSystem, initServant, updateWealth, getGameState, resetGameState } from "./initializers";
 
 setInterval(() => {
     Game();
 }, 100); // repeats Game() every 1000ms (1 second)
 
-localStorage.setItem("test", "test");
-var wealth = { value: isNaN(Number(localStorage.getItem("wealth"))) ? 0 : Number(localStorage.getItem("wealth"))}; // sets wealth to be an object, which can be safely passed by reference into initCoin
-initCoin(wealth);
-initBuildingHover();
-initSaveSystem(wealth);
+var lastTime = 0 // timestamp of last time we updated wealth
+const gameState = getGameState();
+console.log('gameState gotten')
 
+initCoin(gameState.wealth);
+initBuildingHover();
+initSaveSystem(gameState);
+initServant(gameState.wealth, gameState.cps, gameState.numServants, gameState.servantCost);
+
+
+const resetButton = document.getElementById('reset');
+resetButton.addEventListener('click', () => {
+    resetGameState(gameState);
+});
 
 function Game() {
-    const h1 = document.getElementById('test');
-    h1.textContent = `Hello King Midas! You have accumulated $${wealth.value}`;
+    const wealthText = document.getElementById('wealthText');
+    wealthText.textContent = `Hello King Midas! You have accumulated $${gameState.wealth.value}`;
+    const cpsText = document.getElementById('cpsText');
+    cpsText.textContent = `Coins per Second: ${gameState.cps.value}`;
+    const servantCostText = document.getElementById('servantCostText');
+    servantCostText.textContent = `Cost: ${gameState.servantCost.value}`
+
+    let currTime = performance.now(); // time in milliseconds since page started
+    if (((currTime - lastTime) / 1000) >= 1) { // its been 1 second since we updated wealth
+        updateWealth(gameState.wealth, gameState.cps);
+        lastTime = currTime;
+    }
 }
--- src/initializers.js
+++ src/initializers.js
@@ -27,9 +27,55 @@ export function initCoin(wealth) {
     }
 }
 
-export function initSaveSystem(wealth) {
+export function initSaveSystem(gameState) {
     window.addEventListener('beforeunload', () => {
         // maybe move this to a saveWealth function which can be called at intervals to backup not just when you close the tab
-        localStorage.setItem('wealth', String(wealth.value));
+        localStorage.setItem('wealth', String(gameState.wealth.value));
+        localStorage.setItem('cps', String(gameState.cps.value))
+        localStorage.setItem('numServants', String(gameState.numServants.value))
+        localStorage.setItem('servantCost', String(gameState.servantCost.value))
     });
 }
+
+export function initServant(wealth, cps, numServant, cost) {
+    let effect = 1; // cps increase
+    const servant = document.getElementById('servant')
+    servant.addEventListener('click', () => {
+        if (wealth.value >= cost.value) {
+            numServant.value++;
+            wealth.value -= cost.value;
+            cost.value = Math.round(cost.value * 1.15);
+            cps.value += effect;
+        }
+    });    
+}
+
+export function updateWealth(wealth, cps) {
+    wealth.value += cps.value;
+}
+
+export function getGameState() {
+    const gameState = {
+        wealth: { value: isNaN(Number(localStorage.getItem("wealth"))) ? 0 : Number(localStorage.getItem("wealth"))}, // sets wealth to be an object, which can be safely passed by reference into initCoin
+        cps: { value: isNaN(Number(localStorage.getItem("cps"))) ? 0 : Number(localStorage.getItem("cps"))},
+        numServants: { value: isNaN(Number(localStorage.getItem("numServant"))) ? 0 : Number(localStorage.getItem("numServant"))},
+        servantCost : { value: isNaN(Number(localStorage.getItem("servantCost"))) ? 10 : Number(localStorage.getItem("servantCost"))}
+    }
+    return gameState;
+}
+
+export function resetGameState(gameState) {
+    // const keys = Object.keys(gameState);
+    // for (let i = 0; i < keys.length; i++) {
+    //     gameState[keys[i]].value = 0;
+    //     // this will need to be changed once gameState is keeping track of stuff that isn't just numbers
+    // }
+    localStorage.setItem('wealth', '0');
+    localStorage.setItem('cps', '0')
+    localStorage.setItem('numServants', '0')
+    localStorage.setItem('servantCost', '10')
+    gameState.wealth.value = 0;
+    gameState.cps.value = 0;
+    gameState.numServants.value = 0;
+    gameState.servantCost.value = 10;
+}   
